ca4db6d1d333e670ebaf07aa1c86c020db844974
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-19 14:46:45 +0100

    finished install file

--- install.php
+++ install.php
@@ -1,2 +1,13 @@
 <?php
-$connection = new PDO("mysql:host=$host", $username, $password, $options);
+
+
+require "config.php";
+
+try {
+    $connection = new PDO("mysql:host=$host", $username, $password, $options);
+    $query = file_get_contents("data/init.sql");
+    $connection->exec($query);
+    echo "Init Successful";
+} catch(PDOException $error) {
+    echo $query . "Oh no oh god oh lord help whats wrong: " . $error->getMessage();
+}

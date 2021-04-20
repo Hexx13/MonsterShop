01841572377c2b1fb534530d632a8264d0556427
Hexx13 <valv.main@gmail.com>
2021-04-20 20:22:57 +0100

    moved createID function from Account.php to Database.php

--- php/Account.php
+++ php/Account.php
@@ -17,9 +17,7 @@ class Account
         include_once "Database.php";
         $link = Database::createConnection();
         $user = array($accountUsername,$accountPassword,$accountEmail,$firstName,$lastName);
-
-
-        $id  = Account::createID("accountId", "account");
+        $id  = Database::createID("accountId", "account");
 
         $sql = "INSERT INTO account (accountId ,accountUsername, accountPassword, accountEmail, firstName, lastName)
             VALUES  ($id ,'$accountUsername', '$accountPassword', '$accountEmail', '$firstName', '$lastName');";
@@ -27,21 +25,7 @@ class Account
         $statement->execute($user);
     }
 
-    /**
-     * This is a rudimentary version for generating the id in a table provided using a primary key provided
-     * and returns the next available id in the table
-     * @param $pk - takes the primary key name
-     * @param $table - takes the table name
-     * @return int|mixed - returns the next available id
-     */
-    public static function createID($pk, $table){
-        include_once "Database.php";
-        $link = Database::createConnection();
 
-        $sql = "SELECT $pk FROM $table ORDER BY $pk DESC LIMIT 1";
-        $id = $link->query($sql)->fetch();
-        return $id+1;
-    }
 
 
     public static function login($username, $password){
--- php/Database.php
+++ php/Database.php
@@ -19,7 +19,21 @@ class Database
         }
     }
 
+    /**
+     * This is a rudimentary version for generating the id in a table provided using a primary key provided
+     * and returns the next available id in the table
+     * @param $pk - takes the primary key name
+     * @param $table - takes the table name
+     * @return int|mixed - returns the next available id
+     */
+    public static function createID($pk, $table){
+        include_once "Database.php";
+        $link = self::createConnection();
 
+        $sql = "SELECT $pk FROM $table ORDER BY $pk DESC LIMIT 1";
+        $id = $link->query($sql)->fetch();
+        return $id+1;
+    }
 
 
 }

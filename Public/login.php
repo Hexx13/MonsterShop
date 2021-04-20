429b518ed269361c67e3bbfd0b69855772ff50f4
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-20 19:23:52 +0100

    Added log-in form and php request action

--- Public/login.php
+++ Public/login.php
@@ -1,1 +1,21 @@
+<body>
+
+<form action="login" method="POST">
+    <label for="username">Username</label>
+    <input type="text"  placeholder="Username: " name="username"><br>
+
+    <label for="password">password</label>
+    <input type="password" placeholder="Password:" name="password"><br>
+
+    <input type="submit" value="Sign-Up"><br>
+</form>
+
 <?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include_once "../php/Account.php";
+    Account::login($_REQUEST['username'],$_REQUEST['password']);
+}
+?>
+
+</body>

3a74ace5e3689bf0da3a3c1d98b02b6010bc0780
Hexx13 <valv.main@gmail.com>
2021-04-20 20:14:57 +0100

    Added attemptLogin function

--- php/Account.php
+++ php/Account.php
@@ -47,6 +47,11 @@ class Account
     public static function login($username, $password){
         include_once "Database.php";
         $link = Database::createConnection();
+
+    }
+
+    public static function attemptLogin($username, $password){
+        
     }
 
 }

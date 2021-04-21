3292183f2fb1697536b8c5866aa5ffd72e1ce731
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-21 02:12:12 +0100

    createted forms for changing main account details

--- Public/account.php
+++ Public/account.php
@@ -3,37 +3,45 @@
     <?php session_start(); ?>
 </head>
 <body>
+<div>
+    <h1>Change account details</h1>
+    <form action="account.php" method="post">
+        <label for="username">Username</label>
+        <input type="text"  placeholder="Username: " name="username">
+        <input type="submit" value="change" name="submit">
+        <br>
+    </form>
 
-<form action="account.php" method="post">
-    <label for="username">Username</label>
-    <input type="text"  placeholder="Username: " name="username"><br>
+    <form action="account.php" method="post">
+        <label for="firstName">First Name</label>
+        <input type="text"  placeholder="First name:" name="firstName">
+        <input type="submit" value="change" name="submit">
+        <br>
+    </form>
+
+    <form action="account.php" method="post">
+        <label for="lastName">Last Name</label>
+        <input type="text" placeholder="Last name:" name="lastName">
+        <input type="submit" value="change" name="submit">
+        <br>
+    </form>
+
+    <form action="account.php" method="post">
+        <label for="email">Email </label>
+        <input type="text" placeholder="Email:" name="email">
+        <input type="submit" value="change" name="submit">
+        <br>
+    </form>
+
+    <form action="account.php" method="post">
+        <label for="password">password</label>
+        <input type="password" placeholder="Password:" name="password">
+        <input type="submit" value="change" name="submit">
+        <br>
+    </form>
 
 
-</form>
-
-<form action="account.php" method="post">
-    <label for="firstName">First Name</label>
-    <input type="text"  placeholder="First name:" name="firstName"><br>
-
-
-</form>
-
-<form action="account.php" method="post">
-    <label for="lastName">Last Name</label>
-    <input type="text" placeholder="Last name:" name="lastName"><br>
-
-</form>
-
-<form action="account.php" method="post">
-    <label for="email">Email </label>
-    <input type="text" placeholder="Email:" name="email"><br>
-</form>
-
-<form action="account.php" method="post">
-    <label for="password">password</label>
-    <input type="password" placeholder="Password:" name="password"><br>
-</form>
-
+</div>
 
 
 

01a93db45751c3f06bf7204ec05fee0655a7aa84
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-21 02:10:34 +0100

    createted forms for changing main account details

--- Public/account.php
+++ Public/account.php
@@ -1,1 +1,44 @@
-<?php
+<html>
+<head>
+    <?php session_start(); ?>
+</head>
+<body>
+
+<form action="account.php" method="post">
+    <label for="username">Username</label>
+    <input type="text"  placeholder="Username: " name="username"><br>
+
+
+</form>
+
+<form action="account.php" method="post">
+    <label for="firstName">First Name</label>
+    <input type="text"  placeholder="First name:" name="firstName"><br>
+
+
+</form>
+
+<form action="account.php" method="post">
+    <label for="lastName">Last Name</label>
+    <input type="text" placeholder="Last name:" name="lastName"><br>
+
+</form>
+
+<form action="account.php" method="post">
+    <label for="email">Email </label>
+    <input type="text" placeholder="Email:" name="email"><br>
+</form>
+
+<form action="account.php" method="post">
+    <label for="password">password</label>
+    <input type="password" placeholder="Password:" name="password"><br>
+</form>
+
+
+
+
+
+
+
+</body>
+</html>

ce6c485e49acfd66ad9175a2bec7b19be5bb412e
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-24 12:56:12 +0100

    added form handling

--- Public/account.php
+++ Public/account.php
@@ -11,11 +11,11 @@
     <h1>Change account details</h1>
     <?php
     include_once "../php/LayoutUtil.php";
-    LayoutUtil::createAccountDetailForm("account.php", $accDetails['accountUsername'],"username","Username: ");
+    LayoutUtil::createAccountDetailForm("account.php", $accDetails['accountUsername'],"accountUsername","Username: ");
     LayoutUtil::createAccountDetailForm("account.php", $accDetails['firstName'],"firstName","First Name: ");
     LayoutUtil::createAccountDetailForm("account.php", $accDetails['lastName'],"lastName","Last Name: ");
-    LayoutUtil::createAccountDetailForm("account.php", $accDetails['accountEmail'], "email","Email: ");
-    LayoutUtil::createAccountDetailForm("account.php", $accDetails['accountPassword'], "password","Password: ");
+    LayoutUtil::createAccountDetailForm("account.php", $accDetails['accountEmail'], "accountEmail","Email: ");
+    LayoutUtil::createAccountDetailForm("account.php", $accDetails['accountPassword'], "accountPassword","Password: ");
 
     ?>
 
@@ -24,7 +24,9 @@
 
 
 <?php
-if($_REQUEST)
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ Account::changeDetail($_REQUEST['secret'], 'account', $_SESSION['username'], $_REQUEST[$_REQUEST['secret']]);
+}
 
 
 ?>

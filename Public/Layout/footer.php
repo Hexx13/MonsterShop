9769c4190c0429b26cfda1e5b30667956cee110b
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-27 16:20:05 +0100

    Added relevand html to footer.php for the footer

--- Public/Layout/footer.php
+++ Public/Layout/footer.php
@@ -1,1 +1,11 @@
-<footer></footer>
+<footer>
+    <div class="leftFoot">
+        <div class="footLinks">
+
+        </div>
+        <div class="footText">
+            Blah Blah Blah Copyright Stuff Privacy Policy and Lorem Ipsum Dolor Intenso Sit Amet
+        </div>
+    </div>
+    <div class="rightFoot"></div>
+</footer>

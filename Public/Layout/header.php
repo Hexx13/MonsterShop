964bad121b624b525febd6feaa14cbecd3746a1b
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-27 16:15:46 +0100

    added relevant html tags for the header

--- Public/Layout/header.php
+++ Public/Layout/header.php
@@ -1,1 +1,12 @@
-<header></header>
+<header>
+    <div class="rightHead">
+        <a>Home</a>
+        <a>Store</a>
+        <a>About</a>
+    </div>
+    <div class="headSpacer"></div>
+    <div class="leftHead">
+        <a>shopicon</a>
+        <a>accounticon</a>
+    </div>
+</header>

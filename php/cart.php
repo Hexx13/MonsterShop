d5bc77bdb0611bc2ec3ea3b1a5362f5babe10743
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-04-28 15:54:33 +0100

    created main 3 cart functions

--- php/cart.php
+++ php/cart.php
@@ -3,5 +3,28 @@
 
 class cart
 {
+    public static function changeAmountCart($productId,$newAmount){
+        $cart = $_SESSION['cart'];
+        foreach ($cart as $product){
+            if($product['productId'] == $productId){
+                $product['amount'] = $newAmount;
+            }
+        }
+        $_SESSION['cart'] = $cart;
+    }
+
+    public static function removeFromCart($index){
+        unset($_SESSION['cart'][$index]);
+    }
+
+
+    public static function addToCart($prodId, $amount){
+        if (isset($_REQUEST['id'], $_REQUEST['amount'])) {
+            $arr= array();
+            $arr += array("productId" => $prodId, "amount" => $amount, "index" => count($arr));
+            $_SESSION['cart'] =  $arr;
+        }
+    }
+
 
 }

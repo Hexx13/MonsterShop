69df29215a76074aa7483feb6301e99fa69a4a26
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2021-05-12 05:24:44 +0100

    Implemented confirm email and password forms

--- Public/signup.php
+++ Public/signup.php
@@ -3,15 +3,9 @@
     <style>@import "css/stylesheet.css"; </style>
 </head>
 <body>
-<form action="">
-    <label for="pwd">Password:</label>
-    <input type="password" id="pwd" name="pwd" required pattern=".{8,}" title="Eight or more characters">
-    <input type="submit">
-</form>
-
     <!-- TODO
                 * Elaborate on the username validation to validate alphanumeric and not allow symbols
-                * Implement a second confirm email form
+                *add title tags explaining patterns
      -->
 <form action="signup.php" method="POST">
     <label for="username">Username</label>
@@ -27,8 +21,15 @@
     <label for="email">Email </label>
     <input type="text" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}" placeholder="Email:" name="email"><br>
 
+    <label for="email">Confirm Email </label>
+    <input type="text" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}" placeholder="Confirm Email:" name="emailConf"><br>
+
+
     <label for="password">password</label>
     <input type="password"  required pattern=".{3,}" placeholder="Password:" name="password"><br>
+
+    <label for="password">Confirm password</label>
+    <input type="password"  required pattern=".{3,}" placeholder="Confirm Password:" name="passwordConf"><br>
 
     <input type="submit" value="Sign-Up"><br>
     <label> Have account?</label>
